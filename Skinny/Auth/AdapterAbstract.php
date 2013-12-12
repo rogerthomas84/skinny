f078b8a11d017ba9fd6d004a1adcf2f4f5339083
Roger Thomas <rogere84@gmail.com>
2013-12-12 21:24:54 +0000

    Adds a simple auth service and base adapter.

---
+++ Skinny/Auth/AdapterAbstract.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Skinny - a straight forward no-nonsense PHP library
+ *
+ * @author      Roger Thomas <roger.thomas@rogerethomas.com>
+ * @copyright   2013 Roger Thomas
+ * @link        http://www.rogerethomas.com
+ * @license     http://www.rogerethomas.com/license
+ * @since       2.0.6
+ * @package     Skinny
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+namespace Skinny\Auth;
+
+/**
+ * \Skinny\Auth\AdapterAbstract
+ *
+ * This class provides a simple way of constructing adapters to authenticate a user
+ *
+ * @package Skinny
+ * @author  Roger Thomas <roger.thomas@rogerethomas.com>
+ */
+abstract class AdapterAbstract
+{
+    /**
+     * Get the final result
+     * @see Service::SKINNY_AUTH_SERVICE_SUCCESS
+     * @see Service::SKINNY_AUTH_SERVICE_INVALID
+     * @return integer
+     */
+    public abstract function getResult();
+
+    /**
+     * Called on return of SKINNY_AUTH_SERVICE_SUCCESS to retrieve
+     * the array of user identity
+     * @return array
+     */
+    public abstract function getIdentity();
+
+    /**
+     * Called on return of SKINNY_AUTH_SERVICE_SUCCESS to retrieve
+     * the array of user roles
+     * @return array
+     */
+    public abstract function getRoles();
+}
